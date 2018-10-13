9d8a2f769d9c4d442a9190e4340a6a02ce75a4d6
Muyinda Rogers <moverr@gmail.com>
2018-10-13 10:14:03 +0300

    Debugging :

--- app.py
+++ app.py
@@ -4,7 +4,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return '<h1> INDEX PAGE </h1>'
+   return '<h1> HOME PAGE </h1>'
+
+@app.route('/about')
+def about():
+   return '<h1> ABOUT US PAGE </h1>'
+
+
+@app.route('/contact')
+def contact():
+   return '<h1> CONTACT US</h1>'
+
+@app.route('/register')
+def contact():
+   return '<h1> REGISTER</h1>'
+
 
 
 
