997c1e19a6ad06f8d14f4c62ab92005a622deea1
Muyinda Rogers <moverr@gmail.com>
2018-10-13 18:31:22 +0300

    Changes : 10.13.2018

--- app.py
+++ app.py
@@ -19,4 +19,8 @@ api.add_resource(TodoSimple,  '/todo','/todo/<int:todo_id>')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        debug=True,
+        port=app.config['PORT'] if 'PORT' in app.config else 5000,
+        host=app.config['BIND'] if 'BIND' in app.config else '127.0.0.1'
+        )
