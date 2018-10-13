be92c0397cd40d7c9f29c70cbd73770c4cac43d6
Muyinda Rogers <moverr@gmail.com>
2018-10-13 14:50:46 +0300

    including blockchain :

--- app.py
+++ app.py
@@ -1,33 +1,19 @@
 from flask import Flask,jsonify,render_template,request
+from flask_restful import Resource,Api
+from entities import HelloWord,TodoSimple
+ 
+
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-   return  render_template('index.html')
-
-@app.route('/registtration',method=['GET'])
-def getRegistrations():
-   return jsonify('Get all registration in the system')
-
-@app.route('/registtration/<id>',method=['GET'])
-def getRegistrationById(id):
-   return jsonify('Registration by Id')
+api = Api(app)
 
 
-@app.route('/registtration',method=['POST'])
-def register(id):
-   return jsonify('Registration by Id')
+#  pdb.set_trace()
+api.add_resource(HelloWord,'/')
+api.add_resource(TodoSimple,  '/todo','/todo/<int:todo_id>')
+ 
 
-
-
-
-
-
-@app.route('/profile/<profilename>')
-def profile(profilename):
-    return jsonify('profile.html', profile=profilename)
-
+# api.add_resource(TodoSimple,  '/todos/<todo_id>')
 
 
 
